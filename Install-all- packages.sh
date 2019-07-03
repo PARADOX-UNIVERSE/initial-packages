824ca77fba66454214653dfc66e7548d0bcdb21f
PARADOX-UNIVERSE <52336598+PARADOX-UNIVERSE@users.noreply.github.com>
2019-07-03 12:13:39 +0530

    Create Install-all- packages.sh

---
+++ Install-all- packages.sh
@@ -0,0 +1,29 @@
+#This Script is made by PARADOX-UNIVERSE("Ambari Mandal")
+
+yellow = '\033[93m'
+cyan = '\033[96m'
+lgreen = '\033[92m'
+print (lgreen+bold+"     <===[[ coded by P4R4D0X ]]===> \n"+clear)
+print (yellow+bold+"   <---(( search on youtube PARADOX-UNIVERSE ))--> \n"+clear)
+
+
+pkg install root-repo
+Pkg install unstable-repo -y
+Pkg install x11-repo -y
+Pkg install python -y
+Pkg install python2 -y
+Pkg install git -y
+Pkg install wget -y
+Pkg install nano -y
+Pkg install tree -y
+Pkg install w3m -y
+Pkg install termux-api -y
+pkg install proot -y
+pkg install fish-y
+pkg install cowsay -y
+pkg install apache2 -y
+pkg install curl -y
+pkg install libcurl -y
+pkg install chroot -y
+pkg install vim -y
+pkg install bash-completion -y
